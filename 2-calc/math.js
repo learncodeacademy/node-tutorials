3c581e8dc34b03b0a5cb61814e55061e9249bac0
Will Stern <willrstern@gmail.com>
2015-04-29 15:19:01 -0500

    added 2-calc

---
+++ 2-calc/math.js
@@ -0,0 +1,11 @@
+module.exports = {
+  add: function(x, y) {
+    return x+y;
+  },
+  subtract: function(x, y) {
+    return x-y;
+  },
+  squareRoot: function(x) {
+    return Math.sqrt(x);
+  }
+};
